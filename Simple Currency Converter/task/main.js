d1c90206e2030ad5d8a7177a4b6b14bc4b9ec071
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-11-30 12:29:29 +0000

    stage 1

---
+++ Simple Currency Converter/task/main.js
@@ -0,0 +1,13 @@
+// USD exchange rates
+const usd = 1.0;
+const jpy = 113.5;
+const eur = 0.89;
+const rub = 74.36;
+const gbp = 0.75
+
+console.log("Welcome to Currency Converter!");
+console.log(`1 USD equals ${usd} USD`);
+console.log(`1 USD equals ${jpy} JPY`);
+console.log(`1 USD equals ${eur} EUR`);
+console.log(`1 USD equals ${rub} RUB`);
+console.log(`1 USD equals ${gbp} GBP`);
