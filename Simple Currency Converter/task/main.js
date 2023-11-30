926b754d5491da2ac415ce3dda0c33fd1b105693
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-11-30 12:48:03 +0000

    stage 1 improvements

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,13 +1,13 @@
 // USD exchange rates
-const usd = 1.0;
-const jpy = 113.5;
-const eur = 0.89;
-const rub = 74.36;
-const gbp = 0.75
+let exchangeRates = {
+    USD: 1.0,
+    JPY: 113.5,
+    EUR: 0.89,
+    RUB: 74.36,
+    GBP: 0.75
+}
 
 console.log("Welcome to Currency Converter!");
-console.log(`1 USD equals ${usd} USD`);
-console.log(`1 USD equals ${jpy} JPY`);
-console.log(`1 USD equals ${eur} EUR`);
-console.log(`1 USD equals ${rub} RUB`);
-console.log(`1 USD equals ${gbp} GBP`);
+for (rate in exchangeRates) {
+    console.log(`1 USD equals ${exchangeRates[rate]} ${rate}`)
+}
