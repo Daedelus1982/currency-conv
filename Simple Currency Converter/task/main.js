bdc3234ad1c54e75644823798a8b991d648aca15
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-12-06 12:03:58 +0000

    stage 4

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,3 +1,5 @@
+const input = require('sync-input');
+
 // USD exchange rates
 let exchangeRates = {
     USD: 1.0,
@@ -8,6 +10,42 @@ let exchangeRates = {
 }
 
 console.log("Welcome to Currency Converter!");
-for (rate in exchangeRates) {
+for (let rate in exchangeRates) {
     console.log(`1 USD equals ${exchangeRates[rate]} ${rate}`)
 }
+
+while(true) {
+    console.log('What do you want to do?');
+    console.log('1-Convert currencies 2-Exit program');
+    let choice = Number(input());
+    if (isNaN(choice) || choice < 1 || choice > 2) {
+        console.log('Unknown input');
+        continue;
+    }
+    if (choice === 2) {
+        break;
+    }
+
+    console.log('What do you want to convert?');
+    let from = input('From: ').toUpperCase();
+    if (exchangeRates[from] === undefined) {
+        console.log('Unknown currency');
+    } else {
+        let to = input('To: ').toUpperCase();
+        if (exchangeRates[to] === undefined || exchangeRates[from] === undefined) {
+            console.log('Unknown currency');
+        } else {
+            let amount = Number(input('Amount: '));
+            if (isNaN(amount)) {
+                console.log('The amount has to be a number');
+            } else if (amount < 1) {
+                console.log('The amount cannot be less than 1');
+            } else {
+                let result = (amount / exchangeRates[from]) * exchangeRates[to];
+                console.log(`Result: ${amount} ${from} equals ${result.toFixed(4)} ${to}`);
+            }
+        }
+    }
+}
+
+console.log('Have a nice day!');
